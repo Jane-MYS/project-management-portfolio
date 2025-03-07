2d57aeab4bc4329d4346e87a84f2537dbac9fc9b
Jane-MYS <122859958+Jane-MYS@users.noreply.github.com>
2025-03-07 10:52:37 -0800

    Update vite.config.ts
    updated the base to repo name

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/project-management-portfolio/",
+
+  
   server: {
     host: "::",
     port: 8080,
